351f9535e359bc5be006d55ad4fbbc003d21c2ad
ka-cat <3159409764@qq.com>
2025-04-28 10:25:08 +0800

    feat(tomcat):引入tomcat启动项

---
+++ Anykat-application/src/main/java/TomcatApplication.java
@@ -0,0 +1,33 @@
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+
+@Slf4j
+public class TomcatApplication {
+    public static void main(String[] args) throws Exception {
+        // 1. 创建Tomcat实例
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        // 2. 配置webapp目录并获取Context
+        String webappDir = new File("Anykat-application/src/webapp").getAbsolutePath();
+        Context ctx = tomcat.addWebapp("", webappDir);
+
+        // 确保 target/classes 目录被添加到类加载路径中
+        File classesDir = new File("Anykat-application/target/classes");
+        ctx.setReloadable(true); // 允许热加载
+        ctx.setResources(new StandardRoot(ctx) {{
+            addPreResources(new DirResourceSet(this,
+                    "/WEB-INF/classes", classesDir.getAbsolutePath(), "/"));
+        }});
+
+        // 3. 启动服务器
+        tomcat.start();
+        log.info("Server running at http://localhost:{}",tomcat.getConnector().getPort());
+        tomcat.getServer().await();
+    }
+}
