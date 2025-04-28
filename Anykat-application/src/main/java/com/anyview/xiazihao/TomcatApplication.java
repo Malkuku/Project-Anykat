fc1cafaa65d45db70bff57a431500cd26f4e3e92
ka-cat <3159409764@qq.com>
2025-04-28 11:14:44 +0800

    refactor(tomcat):将硬编码变为从yml配置引入

--- Anykat-application/src/main/java/com/anyview/xiazihao/TomcatApplication.java
+++ Anykat-application/src/main/java/com/anyview/xiazihao/TomcatApplication.java
@@ -1,3 +1,6 @@
+package com.anyview.xiazihao;
+
+import com.anyview.xiazihao.config.AppConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
@@ -9,16 +12,24 @@ import java.io.File;
 @Slf4j
 public class TomcatApplication {
     public static void main(String[] args) throws Exception {
+        //加载配置信息
+        final int PORT = AppConfig.getInstance().getTomcat().getPort();
+        final String WEBAPP_DIR = AppConfig.getInstance().getTomcat().getWebappDir();
+        final String CLASSES_DIR = AppConfig.getInstance().getTomcat().getClassesDir();
+        log.info("PORT:{}",PORT);
+        log.info("WEBAPP_DIR:{}",WEBAPP_DIR);
+        log.info("CLASSES_DIR:{}",CLASSES_DIR);
+
         // 1. 创建Tomcat实例
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
+        tomcat.setPort(PORT);
 
         // 2. 配置webapp目录并获取Context
-        String webappDir = new File("Anykat-application/src/webapp").getAbsolutePath();
+        String webappDir = new File(WEBAPP_DIR).getAbsolutePath();
         Context ctx = tomcat.addWebapp("", webappDir);
 
         // 确保 target/classes 目录被添加到类加载路径中
-        File classesDir = new File("Anykat-application/target/classes");
+        File classesDir = new File(CLASSES_DIR);
         ctx.setReloadable(true); // 允许热加载
         ctx.setResources(new StandardRoot(ctx) {{
             addPreResources(new DirResourceSet(this,
