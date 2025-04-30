393e637f918437aedd1b7b203cf5b46a08890812
ka-cat <3159409764@qq.com>
2025-04-30 12:12:23 +0800

    test(Anykat-test)：测试基础类路径扫描功能

---
+++ Anykat-test/src/test/java/com/anyview/xiazihao/TestClassPathScanner.java
@@ -0,0 +1,16 @@
+package com.anyview.xiazihao;
+
+import com.anyview.xiazihao.beanRegistry.Annotation.KatComponent;
+import com.anyview.xiazihao.classPathScanner.ClassPathScanner;
+import org.junit.Test;
+
+import java.util.List;
+
+public class TestClassPathScanner {
+    @Test
+    public void testScanClassesWithAnnotation() {
+        List<Class<?>> annotationClasses = ClassPathScanner.scanClassesWithAnnotation(
+                "com.anyview.xiazihao", KatComponent.class);
+        annotationClasses.forEach(System.out::println);
+    }
+}
