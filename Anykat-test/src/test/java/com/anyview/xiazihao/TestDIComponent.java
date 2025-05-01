643fb08a3de5413c3e6567989824d7f8f2b392ff
ka-cat <3159409764@qq.com>
2025-05-01 09:02:18 +0800

    test(beanRegistry)：测试多包扫描

--- Anykat-test/src/test/java/com/anyview/xiazihao/TestDIComponent.java
+++ Anykat-test/src/test/java/com/anyview/xiazihao/TestDIComponent.java
@@ -2,9 +2,12 @@ package com.anyview.xiazihao;
 
 import com.anyview.xiazihao.beanRegistry.Annotation.KatAutowired;
 import com.anyview.xiazihao.beanRegistry.ContainerFactory;
+import com.anyview.xiazihao.config.AppConfig;
 import com.anyview.xiazihao.service.MovieService;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Set;
 
 public class TestDIComponent {
     private ContainerFactory containerFactory;
@@ -13,7 +16,7 @@ public class TestDIComponent {
     private MovieService movieService;
     @Before
     public void init() throws Exception {
-        containerFactory = new ContainerFactory("com.anyview.xiazihao");
+        containerFactory = new ContainerFactory();
         containerFactory.injectDependencies(this); // 手动触发注入
     }
 
