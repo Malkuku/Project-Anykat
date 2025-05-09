201458e7afad88f0ff87a74c3abcbd8a6edfd899
ka-cat <3159409764@qq.com>
2025-05-09 15:18:57 +0800

    test(TestController)测试基础请求参数，基础Json请求体参数

--- Anykat-management/src/main/java/com/anyview/xiazihao/controller/TestController.java
+++ Anykat-management/src/main/java/com/anyview/xiazihao/controller/TestController.java
@@ -1,9 +1,8 @@
 package com.anyview.xiazihao.controller;
 
 import com.anyview.xiazihao.containerFactory.annotation.KatComponent;
-import com.anyview.xiazihao.controller.annotation.KatController;
-import com.anyview.xiazihao.controller.annotation.KatPathVariable;
-import com.anyview.xiazihao.controller.annotation.KatRequestMapping;
+import com.anyview.xiazihao.controller.annotation.*;
+import com.anyview.xiazihao.entity.test.Movie;
 
 @KatComponent
 @KatController
@@ -17,5 +16,17 @@ public class TestController {
     @KatRequestMapping(path = "/{userId}", method = "GET")
     public String test2(@KatPathVariable("userId") long userId) {
         return "test2: " + userId;
+    }
+
+    @KatRequestMapping(path = "/test3",method = "GET")
+    public String test3(
+            @KatRequestParam("id") Integer id) {
+        return "test3: " + id;
+    }
+
+    @KatRequestMapping(path = "/test4",method = "GET")
+    public String test4(
+            @KatRequestBody() Movie movie){
+        return "test4: " + movie;
     }
 }
