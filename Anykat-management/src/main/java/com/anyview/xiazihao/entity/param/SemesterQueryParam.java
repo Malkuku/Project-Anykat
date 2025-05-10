630e6227f5c39a579ebf0840c9b9b3424d7e7459
ka-cat <3159409764@qq.com>
2025-05-10 15:06:43 +0800

    feat(Semester)学期基础信息查询

---
+++ Anykat-management/src/main/java/com/anyview/xiazihao/entity/param/SemesterQueryParam.java
@@ -0,0 +1,20 @@
+package com.anyview.xiazihao.entity.param;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SemesterQueryParam {
+    private String name;         // 学期名称(模糊查询)
+    private LocalDateTime startTime; // 开始时间
+    private LocalDateTime endTime;   // 结束时间
+    private Integer page = 1;     // 页码(默认1)
+    private Integer pageSize = 10;// 每页条数(默认10)
+    private Integer offset = 0;   // 偏移量
+}
---
+++ Anykat-management/src/main/java/com/anyview/xiazihao/service/impl/SemesterServiceImpl.java
@@ -0,0 +1,34 @@
+package com.anyview.xiazihao.service.impl;
+
+import com.anyview.xiazihao.containerFactory.annotation.KatAutowired;
+import com.anyview.xiazihao.containerFactory.annotation.KatComponent;
+import com.anyview.xiazihao.containerFactory.annotation.KatSingleton;
+import com.anyview.xiazihao.dao.SemesterDao;
+import com.anyview.xiazihao.entity.param.SemesterQueryParam;
+import com.anyview.xiazihao.entity.pojo.Semester;
+import com.anyview.xiazihao.entity.result.PageResult;
+import com.anyview.xiazihao.service.SemesterService;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.util.List;
+
+@KatComponent
+@KatSingleton
+public class SemesterServiceImpl implements SemesterService {
+    @KatAutowired
+    private SemesterDao semesterDao;
+
+    @Override
+    public PageResult<Semester> selectSemesterByPage(SemesterQueryParam param) throws SQLException, FileNotFoundException {
+        Integer total = semesterDao.selectSemesterCount(param);
+        param.setOffset((param.getPage() - 1) * param.getPageSize());
+        List<Semester> semesters = semesterDao.selectSemesterByPage(param);
+        return new PageResult<>(total, semesters);
+    }
+
+    @Override
+    public Semester selectSemesterById(Integer id) throws SQLException, FileNotFoundException {
+        return semesterDao.selectSemesterById(id);
+    }
+}
