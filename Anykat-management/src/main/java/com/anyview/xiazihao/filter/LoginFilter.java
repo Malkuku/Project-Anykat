ca1f67b74357c59df71b36497148ece287af3037
ka-cat <3159409764@qq.com>
2025-05-14 11:22:10 +0800

    fix(LoginFilter)修正对adminToken的错误解析 feat：添加验证失败异常捕获响应

--- Anykat-management/src/main/java/com/anyview/xiazihao/filter/LoginFilter.java
+++ Anykat-management/src/main/java/com/anyview/xiazihao/filter/LoginFilter.java
@@ -3,13 +3,17 @@ package com.anyview.xiazihao.filter;
 import com.anyview.xiazihao.config.AppConfig;
 import com.anyview.xiazihao.entity.context.AdminContext;
 import com.anyview.xiazihao.entity.context.UserContext;
+import com.anyview.xiazihao.entity.exception.ValidationFailedException;
 import com.anyview.xiazihao.entity.pojo.User;
+import com.anyview.xiazihao.entity.result.Result;
 import com.anyview.xiazihao.utils.JwtUtils;
+import com.anyview.xiazihao.utils.ServletUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
@@ -42,76 +46,81 @@ public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        if(!FILTER_OPEN){
-            chain.doFilter(request, response);
-            return;
-        }
-
-        // 放行登录接口
-        if (httpRequest.getRequestURI().contains("/common-users/login") && "POST".equalsIgnoreCase(httpRequest.getMethod())) {
-            chain.doFilter(request, response);
-            return;
-        }
-
-        // 放行获取用户信息接口
-        if (!GET_USER_INFO_OPEN && httpRequest.getRequestURI().contains("/common-users") && "GET".equalsIgnoreCase(httpRequest.getMethod())) {
-            chain.doFilter(request, response);
-            return;
-        }
-
-        //获取请求头的Token
-        Map<String,Object> claims;
-        String token = httpRequest.getHeader("token");
-        log.info("Token:{}", token);
-        //验证Token
-        if (token == null || token.isEmpty()) {
-            throw new ServletException("Token不能为空");
-        }
         try {
-            claims = JwtUtils.parseToken(token);
-        } catch (Exception e) {
-            throw new ServletException("Token错误");
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            if (!FILTER_OPEN) {
+                chain.doFilter(request, response);
+                return;
+            }
+
+            // 放行登录接口
+            if (httpRequest.getRequestURI().contains("/common-users/login") && "POST".equalsIgnoreCase(httpRequest.getMethod())) {
+                chain.doFilter(request, response);
+                return;
+            }
+
+            // 放行获取用户信息接口
+            if (!GET_USER_INFO_OPEN && httpRequest.getRequestURI().contains("/common-users") && "GET".equalsIgnoreCase(httpRequest.getMethod())) {
+                chain.doFilter(request, response);
+                return;
+            }
+
+            //获取请求头的Token
+            Map<String, Object> claims;
+            String token = httpRequest.getHeader("token");
+            log.info("Token:{}", token);
+            //验证Token
+            if (token == null || token.isEmpty()) {
+                throw new ValidationFailedException("Token不能为空");
+            }
+            try {
+                claims = JwtUtils.parseToken(token);
+            } catch (Exception e) {
+                throw new ValidationFailedException("Token错误");
+            }
+            //放行请求
+            log.info("Token验证通过");
+
+            //如果开启用户验证，储存用户上下文
+            if (AUTH_OPEN) {
+                User user = new User();
+                user.setId((Integer) claims.get("id"));
+                user.setUsername((String) claims.get("username"));
+                user.setRole((Integer) claims.get("role"));
+                UserContext.setUser(user);
+            }
+
+            //如果是管理员，验证管理员Token
+            String adminToken = httpRequest.getHeader("adminToken");
+            log.info("adminToken:{}", adminToken);
+            //验证Token
+            if (adminToken == null || adminToken.isEmpty()) {
+                throw new ValidationFailedException("adminToken不能为空");
+            }
+            try {
+                claims = JwtUtils.parseAdminToken(adminToken);
+            } catch (Exception e) {
+                throw new ValidationFailedException("adminToken错误");
+            }
+            //放行请求
+            log.info("adminToken验证通过");
+
+            if (AUTH_OPEN) {
+                User user = new User();
+                user.setId((Integer) claims.get("id"));
+                user.setUsername((String) claims.get("username"));
+                user.setRole((Integer) claims.get("role"));
+                AdminContext.setUser(user);
+            }
+
+
+            chain.doFilter(request, response);
+        }catch (ValidationFailedException e){
+            log.error("过滤器捕获异常: {} {}",e, e.getMessage(),e.getCause());
+            ServletUtils.sendResponse((HttpServletResponse) response, Result.error(e.getMessage()));
         }
-        //放行请求
-        log.info("Token验证通过");
-
-        //如果开启用户验证，储存用户上下文
-         if(AUTH_OPEN){
-             User user = new User();
-             user.setId((Integer) claims.get("id"));
-             user.setUsername((String) claims.get("username"));
-             user.setRole((Integer) claims.get("role"));
-             UserContext.setUser(user);
-         }
-
-        //如果是管理员，验证管理员Token
-        String adminToken = httpRequest.getHeader("adminToken");
-        log.info("adminToken:{}", adminToken);
-        //验证Token
-        if (adminToken == null || adminToken.isEmpty()) {
-            throw new ServletException("adminToken不能为空");
-        }
-        try {
-            claims = JwtUtils.parseToken(adminToken);
-        } catch (Exception e) {
-            throw new ServletException("adminToken错误");
-        }
-        //放行请求
-        log.info("adminToken验证通过");
-
-        if(AUTH_OPEN){
-            User user = new User();
-            user.setId((Integer) claims.get("id"));
-            user.setUsername((String) claims.get("username"));
-            user.setRole((Integer) claims.get("role"));
-            AdminContext.setUser(user);
-        }
-
-
-
-        chain.doFilter(request, response);
     }
+
 
     @Override
     public void destroy() {
